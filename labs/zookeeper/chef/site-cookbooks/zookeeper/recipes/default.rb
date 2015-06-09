6a8a7534c6ae45dba1537b0cb68ab8ebf8b7177a
david.anderson@pobox.com <dave.anderson@bronto.com>
2015-06-09 02:00:41 -0400

    fix checksum typo on JDK tarball

--- labs/zookeeper/chef/site-cookbooks/zookeeper/recipes/default.rb
+++ labs/zookeeper/chef/site-cookbooks/zookeeper/recipes/default.rb
@@ -11,7 +11,7 @@ node.override['java']['install_flavor'] = 'oracle'
 node.override['java']['jdk']['7']['x86_64']['url'] = 'https://s3.amazonaws.com/bronto-tmp/jdk-7u79-linux-x64.tar.gz'
 node.override['java']['jdk']['7']['x86_64']['checksum'] = '29d75d0022bfa211867b876ddd31a271b551fa10727401398295e6e666a11d90'
 node.override['java']['jdk']['8']['x86_64']['url'] = 'https://s3.amazonaws.com/bronto-tmp/jdk-8u45-linux-x64.tar.gz'
-node.override['java']['jdk']['7']['x86_64']['checksum'] = 'f298ca9239051dfddf8642fcc9e264f7fe5af10adb67027feb3a0ed0a1a2316d'
+node.override['java']['jdk']['8']['x86_64']['checksum'] = 'f298ca9239051dfddf8642fcc9e264f7fe5af10adb67027feb3a0ed0a1a2316d'
 node.override['java']['oracle']['accept_oracle_download_terms'] = true
 
 node.override['zookeeper']['version'] = ENV['zk_version'].to_s.empty? ? node[:zookeeper][:version] : ENV['zk_version']
